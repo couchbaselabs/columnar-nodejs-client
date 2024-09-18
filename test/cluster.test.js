0106454da7dc35d54f3d79c860a0355247d66b9e
Mateusz <matt.wozakowski@gmail.com>
2024-09-18 11:56:18 +0100

    Add cluster opts unit tests

--- test/cluster.test.js
+++ test/cluster.test.js
@@ -17,10 +17,10 @@
 
 'use strict'
 
-const assert = require('assert')
-const harness = require('./harness')
+const assert = require('chai').assert
+const H = require('./harness')
 
-const H = harness
+const { PassthroughDeserializer } = require("../lib/deserializers");
 
 describe('#Cluster', function () {
   it('should correctly set timeouts', function () {
@@ -45,6 +45,67 @@ describe('#Cluster', function () {
     assert.equal(cluster.managementTimeout, 80000)
     assert.equal(cluster.queryTimeout, 80000)
     assert.equal(cluster.resolveTimeout, 30000)
+  })
+
+  it('should correctly set all security options', function () {
+    let options = {
+      securityOptions: {
+        trustOnlyCapella: true,
+        trustOnlyPemFile: "pemFile",
+        trustOnlyPemString: "pemString",
+        trustOnlyCertificates: ["cert1", "cert2"],
+        trustOnlyPlatform: true,
+        verifyServerCertificates: false,
+        cipherSuites: ["suite"]
+      }
+    }
+
+    const cluster = H.lib.Cluster.createInstance(
+        H.connStr,
+        H.credentials,
+        options
+    )
+
+    assert.isTrue(cluster._securityOptions.trustOnlyCapella)
+    assert.strictEqual(cluster._securityOptions.trustOnlyPemFile, "pemFile")
+    assert.strictEqual(cluster._securityOptions.trustOnlyPemString, "pemString")
+    assert.deepEqual(cluster._securityOptions.trustOnlyCertificates, ["cert1", "cert2"])
+    assert.isTrue(cluster._securityOptions.trustOnlyPlatform)
+    assert.isFalse(cluster._securityOptions.verifyServerCertificates)
+    assert.deepEqual(cluster._securityOptions.cipherSuites, ["suite"])
+  })
+
+  it('should correctly set dns options', function () {
+    let options = {
+      dnsConfig: {
+        nameserver: "localhost",
+        port: 12345,
+        dnsSrvTimeout: 3000
+      }
+    }
+
+    const cluster = H.lib.Cluster.createInstance(
+        H.connStr,
+        H.credentials,
+        options
+    )
+
+    assert.strictEqual(cluster._dnsConfig.nameserver, "localhost")
+    assert.strictEqual(cluster._dnsConfig.port, 12345)
+    assert.strictEqual(cluster._dnsConfig.dnsSrvTimeout, 3000)
+  })
+
+  it ('should correctly set cluster-level deserializer', function() {
+    let options = {
+      deserializer: new PassthroughDeserializer()
+    }
+
+    const cluster = H.lib.Cluster.createInstance(
+        H.connStr,
+        H.credentials,
+        options
+    )
+    assert.instanceOf(cluster.deserializer, PassthroughDeserializer)
   })
 
   it('should error ops after close and ignore superfluous closes', async function () {
